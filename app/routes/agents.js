81e409060d3d54eafdfaa3b8fe473f121763e680
Caryl Westerberg <cjwest@acm.org>
2016-10-22 09:42:59 -0700

    After ember training

--- app/routes/agents.js
+++ app/routes/agents.js
@@ -1,4 +1,47 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Route.extend({
+
+  model() {
+
+    return $.ajax({
+      url: '/myproxy/tas_console/include/api.php',
+      data: {
+        op: 'get',
+        op2: 'extension',
+        ext_name: 'dash',
+        ext_function: 'dash_image',
+        return_type: 'csv',
+        id: 'Kentfield',
+        other_mode: 'url_encode_separator_|',
+        apipass: '5875',
+        user: 'demo',
+        pass: 'Dem04u!',
+      }
+    }
+
+//'https://demo.theftalertsystem.com:8443/tas_console/include/api.php?op=get&op2=extension&ext_name=dash&ext_function=dash_image&return_type=csv&id=Kentfield&other_mode=url_encode_separator_|&apipass=5875&user=demo&pass=Dem04u!');
+//'/myproxy/tas_console/include/api.php?op=get&op2=extension&ext_name=dash&ext_function=dash_image&return_type=csv&id=Kentfield&other_mode=url_encode_separator_|&apipass=5875&user=demo&pass=Dem04u!');
+    //return this.store.findRecord('article', params.id);
+  }
 });
+/*
+
+                    $.ajax
+                    ({
+                        type: "POST",
+                        //the url where you want to sent the userName and password to
+                        url: "http://notgiven",
+                        dataType: 'json',
+                        async: false,
+                        //json object to sent to the authentication url
+                        //data: {"username": "' + userName + '", "password" : "' + password + '"},
+                     data: { username: "user1", password: "test1" },,
+                        success: function (data) {
+                            //do any process for successful authentication here
+                            alert('Login status: ' + data.status);
+                        }
+                     })
+                };
+*/
---
+++ server/proxies/proxy.js
@@ -0,0 +1,21 @@
+/*jshint node:true*/
+var proxyPath = '/myproxy';
+
+module.exports = function(app) {
+  // For options, see:
+  // https://github.com/nodejitsu/node-http-proxy
+  var proxy = require('http-proxy').createProxyServer({
+    changeOrigin: true
+  });
+
+  proxy.on('error', function(err, req) {
+    console.error(err, req.url);
+  });
+
+  app.use(proxyPath, function(req, res, next){
+    // include root path in proxied request
+    //req.url = proxyPath + '/' + req.url;
+    //proxy.web(req, res, { target: 'https://www.google.com/' });
+    proxy.web(req, res, { target: 'https://demo.theftalertsystem.com:8443' });
+  });
+};
